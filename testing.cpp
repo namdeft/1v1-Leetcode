95d6042d5d28d8ce357906225ae8b6e54c628b0c
namdeft <dinhnam2306@gmail.com>
2023-06-10 23:29:14 +0700

    test file

--- testing.cpp
+++ testing.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main() {
     vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    int size = nums.size();
+    int left = 0;
+    int right = size - 1;
+
+    while (left < right) {
+        int mid = (left + right) / 2;
+
+        if (mid < target) {
+        }
+    }
 
     return 0;
 }

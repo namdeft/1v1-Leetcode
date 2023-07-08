0fc451137a788f45382f910cf3e1b0ba70a8f693
namdeft <dinhnam2306@gmail.com>
2023-07-08 13:33:28 +0700

    Search Insert Position

---
+++ Binary Search/Search Insert Position/solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int mid;
+
+    if (target > nums[right]) {
+        return right + 1;
+    }
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (target == nums[mid]) {
+            return mid;
+        }
+
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 6};
+    cout << searchInsert(nums, 4) << endl;
+
+    return 0;
+}

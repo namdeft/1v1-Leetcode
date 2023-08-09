b4684661b1bfc7ee9606ab0e8ea85ecfa8f55d1c
namdeft <dinhnam2306@gmail.com>
2023-08-09 13:34:30 +0700

    Design Parking System

--- Design/Design Parking System/solution.ts
+++ Design/Design Parking System/solution.ts
@@ -1,11 +1,38 @@
 class ParkingSystem {
+  big: number;
+  medium: number;
+  small: number;
   
   constructor(big: number, medium: number, small: number) {
-      
+      this.big = big;
+      this.medium = medium;
+      this.small = small;
   }
 
   addCar(carType: number): boolean {
-      
+    switch(carType) {
+      case 1: {
+        if (this.big) {
+          this.big--;
+          return true;
+        }else return false;
+      }
+
+      case 2: {
+        if (this.medium) {
+          this.medium--;
+          return true;
+        }else return false;
+      }
+
+      case 3: {
+        if (this.small) {
+          this.small--;
+          return true;
+        }else return false;
+
+      }
+    }
   }
 }
 

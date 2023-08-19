6165b1a4d50f1431b9ff21310e43e20d3bddb89c
namdeft <dinhnam2306@gmail.com>
2023-08-19 23:18:41 +0700

    LRU Cache

--- Design/LRU Cache/solution.ts
+++ Design/LRU Cache/solution.ts
@@ -11,10 +11,3 @@ class LRUCache {
 
   }
 }
-
-/**
-* Your LRUCache object will be instantiated and called as such:
-* var obj = new LRUCache(capacity)
-* var param_1 = obj.get(key)
-* obj.put(key,value)
-*/

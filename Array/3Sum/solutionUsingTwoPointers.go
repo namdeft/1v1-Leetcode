1f4ed4fc51b5a6088f22be81edd4d5e1fab15be9
namdeft <dinhnam2306@gmail.com>
2024-02-05 18:56:13 +0700

    3Sum

--- Array/3Sum/solutionUsingTwoPointers.go
+++ Array/3Sum/solutionUsingTwoPointers.go
@@ -1,3 +1,37 @@
 func threeSum(nums []int) [][]int {
+	var results [][]int
+	sort.Ints(nums)
 
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(nums)-1
+
+		for left < right {
+			target := nums[i] + nums[left] + nums[right]
+
+			if target == 0 {
+				results = append(results, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+
+			} else if target < 0 {
+				left++
+			} else if target > 0 {
+				right--
+			}
+		}
+	}
+
+	return results
 }

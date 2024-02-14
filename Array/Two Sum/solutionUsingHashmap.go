f4c1dfd52f391e0acc534b46094951663632fa6a
namdeft <dinhnam2306@gmail.com>
2024-02-14 23:52:36 +0700

    Two sum

--- Array/Two Sum/solutionUsingHashmap.go
+++ Array/Two Sum/solutionUsingHashmap.go
@@ -1,3 +1,16 @@
 func twoSum(nums []int, target int) []int {
+	var results []int
+	numsMap := make(map[int]int)
 
+	for index, value := range nums {
+		numNeeds := target - value
+
+		if pairNumNeeds, ok := numsMap[numNeeds]; ok {
+			return append(results, index, pairNumNeeds)
+		}
+
+		numsMap[value] = index
+	}
+
+	return results
 }

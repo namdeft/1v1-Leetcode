5bb32279a3023b01bca855770f1be6ae672128b4
namdeft <dinhnam2306@gmail.com>
2024-02-17 23:47:01 +0700

    Two sum

--- Array/Two Sum/solution.go
+++ Array/Two Sum/solution.go
@@ -1,13 +1,13 @@
 func twoSum(nums []int, target int) []int {
-	numsLength := len(nums)
+	var results []int
 
-	for i := 0; i < numsLength-1; i++ {
-		for j := i + 1; j < numsLength; j++ {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
-				return []int{i, j}
+				return append(results, i, j)
 			}
 		}
 	}
 
-	return []int{}
+	return results
 }

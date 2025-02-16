e6d0e1b8ce569e0582109e4949958ccb01426912
namdeft <dinhnam2306@gmail.com>
2025-02-16 23:45:29 +0700

    Valid Palindrome

--- String/Valid Palindrome/solution.go
+++ String/Valid Palindrome/solution.go
@@ -1,27 +1,32 @@
-func isValid(s string) bool {
-	stack := make([]rune, 0)
-	mp := map[rune]rune{
-		'}': '{',
-		')': '(',
-		']': '[',
-	}
-
-	for _, char := range s {
-		if char == '(' || char == '{' || char == '[' {
-			stack = append(stack, char)
-		}
-
-		if val, ok := mp[char]; ok {
-			if len(stack) > 0 && stack[len(stack)-1] == val {
-				stack = stack[0 : len(stack)-1]
-			} else {
-				return false
-			}
-		}
-	}
-
-	if len(stack) == 0 {
+func isPalindrome(s string) bool {
+	if len(s) == 0 {
 		return true
 	}
+
+	pointer1 := 0
+	pointer2 := len(s) - 1
+	for pointer1 < pointer2 {
+		for pointer1 < pointer2 && !isAlphabet(s[pointer1]) {
+			pointer1++
+		}
+		for pointer1 < pointer2 && !isAlphabet(s[pointer2]) {
+			pointer2--
+		}
+		if unicode.ToLower(rune(s[pointer1])) != unicode.ToLower(rune(s[pointer2])) {
+			return false
+		}
+
+		pointer1++
+		pointer2--
+	}
+
+	return true
+}
+
+func isAlphabet(char byte) bool {
+	if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
+		return true
+	}
+
 	return false
 }

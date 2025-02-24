617f7d103148e016cbbaf2d7dfcb4d1c44e2bac1
namdeft <dinhnam2306@gmail.com>
2025-02-25 00:25:39 +0700

    Insert Interval

--- Array/Insert Interval/solution.go
+++ Array/Insert Interval/solution.go
@@ -1,3 +1,46 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
+	var result [][]int
+	var i int
 
+	for i < len(intervals) && intervals[i][1] < newInterval[0] {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(intervals[i][0], newInterval[0])
+		newInterval[1] = max(intervals[i][1], newInterval[1])
+		i++
+	}
+	result = append(result, newInterval)
+
+	for i < len(intervals) {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	return result
 }
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].

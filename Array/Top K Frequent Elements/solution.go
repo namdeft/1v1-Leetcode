50d93775ed5c022b7b966f8508d01d02247a3d0e
namdeft <dinhnam2306@gmail.com>
2025-02-28 00:05:11 +0700

    Top K Frequent Elements

--- Array/Top K Frequent Elements/solution.go
+++ Array/Top K Frequent Elements/solution.go
@@ -1,3 +1,26 @@
 func topKFrequent(nums []int, k int) []int {
+	var result []int
+	var mp map[int]int
+	for _, num := range nums {
+		mp[num]++
+	}
 
+	var tempArr []Pair
+	for k, v := range mp {
+		tempArr = append(tempArr, Pair{num: k, count: v})
+	}
+	sort.Slice(tempArr, func(i, j int) bool {
+		return tempArr[i].Count > tempArr[j].Count
+	})
+
+	for i := 0; i < k; i++ {
+		result = append(result, tempArr[i].Num)
+	}
+
+	return result
+}
+
+type Pair struct {
+	Num   int
+	Count int
 }

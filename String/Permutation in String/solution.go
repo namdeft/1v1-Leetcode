10e2aa5071e3eac058af43d4123e2d363610aa4e
namdeft <dinhnam2306@gmail.com>
2025-03-06 22:32:17 +0700

    Permutation in String

--- String/Permutation in String/solution.go
+++ String/Permutation in String/solution.go
@@ -1,3 +1,33 @@
 func checkInclusion(s1 string, s2 string) bool {
+	mpS1Count := make(map[rune]int)
+	for i, _ := range s1 {
+		mpS1Count[s1[i]]++
+	}
+	mpS2Count := make(map[rune]int)
+	left := 0
+	matches := 0
 
+	for right := 0; right < len(s2); right++ {
+		char := s2[right]
+		mpS2Count[char]++
+		if mpS1Count[char] == mpS2Count[char] {
+			matches++
+		}
+
+		if right-left == len(s1) {
+			char = s2[left]
+			if mpS1Count[char] == mpS2Count[char] {
+				matches--
+			}
+			mpS2Count[char]--
+
+			left++
+		}
+
+		if matches == len(mpS1Count) {
+			return true
+		}
+	}
+
+	return false
 }

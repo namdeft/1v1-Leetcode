1acd6dce1d5a9f5de8141858532d2e51872d1226
namdeft <dinhnam2306@gmail.com>
2025-03-24 22:08:05 +0700

    Best Time to Buy and Sell Stock II

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock II/solution.go
@@ -0,0 +1,12 @@
+func maxProfit(prices []int) int {
+	var result int
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	for i := 1; i < len(prices); i++ {
+		currentProfit := prices[i] - prices[i-1]
+	}
+
+	return result
+}

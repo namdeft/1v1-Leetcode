466d27e21e3a2d8b3294c83d3e396514805e3c3e
namdeft <dinhnam2306@gmail.com>
2025-03-26 22:57:30 +0700

    Fibonacci Number

---
+++ Array/Fibonacci Number/solution.go
@@ -0,0 +1,17 @@
+func fib(n int) int {
+	var result int
+	if n <= 1 {
+		return n
+	}
+
+	if val, ok := mp[n]; ok {
+		return val
+	}
+
+	mp[n] = fib(n-1) + fib(n-2)
+	result = mp[n]
+
+	return result
+}
+
+var mp = make(map[int]int)

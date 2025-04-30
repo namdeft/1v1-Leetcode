58bf33b25f86d3fa4503fd8a253da078dedfbe19
namdeft <dinhnam2306@gmail.com>
2025-04-30 15:56:43 +0700

    Min Stack

---
+++ Stack/Min Stack/solution.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+
+        stack.push(val);
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        int val = stack.peek();
+
+        return val;
+    }
+
+    public int getMin() {
+        int val = minStack.peek();
+
+        return val;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
